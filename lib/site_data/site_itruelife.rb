1d6f8bfbe3cfc201e3862e51142bd27efc0e2da0
fushang318 <fushang318@gmail.com>
2015-04-08 10:29:12 +0800

    修复 凡夫 获取图片

--- lib/site_data/site_itruelife.rb
+++ lib/site_data/site_itruelife.rb
@@ -4,9 +4,9 @@ class SiteItruelife
   def self.parse(url)
     fetch(url) do
       title 'div[@class="detail-main"] h1'
-      # image_url 'div[@id="wrap"] img', :text do |h|
-      #   h.first['src']
-      # end
+      image_url 'a[@class="cloud-zoom"]', :text do |h|
+        h.first["href"]
+      end
       price 'ins[@class="ff_arial"]'
     end
 
