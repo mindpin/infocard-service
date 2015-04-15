9d953c7edb4e2c425eaa90e171cd0996fd91db05
arlyxiao <kingla_pei@163.com>
2015-04-15 11:21:35 +0800

    fix price in taobao

--- lib/site_data/site_taobao.rb
+++ lib/site_data/site_taobao.rb
@@ -7,9 +7,25 @@ class SiteTaobao
       image_url  'script' do |h|
         h.to_s.split('auctionImages:[')[1].split(',')[0].gsub('"', '').strip
       end
-      price 'strong[@id="J_StrPrice"] em[@class="tb-rmb-num"]'
+
+      price do |h|
+        pid = URI.parse(URI.encode(url)).query.split("&").select do |str|
+          str.match(/id=.*/)
+        end[0].gsub("id=","")
+        mechanize = Mechanize.new
+        mechanize.keep_alive = false
+        mechanize.user_agent_alias = 'Windows Chrome'
+        price_url = "http://mdskip.taobao.com/core/initItemDetail.htm?itemId=#{pid}"
+        page = mechanize.get(price_url, parameters = [], referer = url )
+        parse_price_by_reg(page.body)
+      end
     end
 
+  end
+
+
+  def self.parse_price_by_reg(str)
+    str.scan(/\"price\"\:\"([^,]*)\",/).flatten.map{|p|p.to_f}.min
   end
 
 end
