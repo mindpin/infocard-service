c3b829efd06131dc4d58248dc9925fd551af6c81
arlyxiao <kingla_pei@163.com>
2015-04-23 13:30:12 +0800

    fix amazon

--- lib/site_data/site_amazon.rb
+++ lib/site_data/site_amazon.rb
@@ -4,9 +4,14 @@ class SiteAmazon
   def self.parse(url)
     fetch(url) do
       title 'span[@id="productTitle"]'
-      image_url 'img[@id="landingImage"]' do |h|
-        h.first['data-old-hires']
+      # image_url 'img[@id="landingImage"]' do |h|
+      #   h.first['data-old-hires']
+      # end
+
+      image_url 'script' do |h|
+        h.to_s.split('"large":"')[1].split('",')[0]
       end
+
       price 'span[@id="priceblock_ourprice"]'
       from {"亚马逊（中国）"}
     end
